44b0441531ec7ccb4c418f8b1c85a9de336f3272
marcin10lw <marcin10lw@wp.pl>
2022-11-12 21:16:57 +0100

    organized js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,43 @@
     },
   ];
 
+  const setFocus = () => {
+    const inputElement = document.querySelector(".js-inputTask");
+    inputElement.focus();
+  };
+
+  const clearField = () => {
+    let inputContent = document.querySelector(".js-inputTask");
+    inputContent.value = "";
+  };
+
+  const addNewTask = (event) => {
+    event.preventDefault();
+    setFocus();
+
+    const inputContent = document.querySelector(".js-inputTask").value.trim();
+
+    if (inputContent === "") {
+      return;
+    }
+
+    tasks.push({ content: inputContent });
+    render();
+
+    clearField();
+  };
+
+  const deleteTask = () => {
+    const deleteButtonElements = document.querySelectorAll(".js-deleteButton");
+
+    deleteButtonElements.forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        tasks.splice(index, 1);
+        render();
+      });
+    });
+  };
+
   const render = () => {
     const listOfTasksElement = document.querySelector(".js-listOfTasks");
 
@@ -29,45 +66,14 @@
 
     listOfTasksElement.innerHTML = list;
 
-    const deleteButtonElements = document.querySelectorAll(".js-deleteButton");
-    deleteButtonElements.forEach((deleteButton, index) => {
-      deleteButton.addEventListener("click", () => {
-        tasks.splice(index, 1);
-        render();
-      });
-    });
-  };
-
-  const addNewTask = (event) => {
-    event.preventDefault();
-
-    const inputContent = document.querySelector(".js-inputTask").value.trim();
-
-    if (inputContent === "") {
-      return;
-    }
-
-    tasks.push({ content: inputContent });
-    render();
-  };
-
-  const setFocus = () => {
-    const inputElement = document.querySelector(".js-inputTask");
-    inputElement.focus();
-  };
-
-  const clearField = () => {
-    let inputContent = document.querySelector(".js-inputTask");
-    inputContent.value = "";
+    deleteTask();
   };
 
   const init = () => {
     render();
 
     const buttonElement = document.querySelector(".js-inputButton");
-    buttonElement.addEventListener("click", setFocus);
     buttonElement.addEventListener("click", addNewTask);
-    buttonElement.addEventListener("click", clearField);
   };
 
   init();
